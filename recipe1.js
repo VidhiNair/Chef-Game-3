3ccff1e83406556965214e3f075a2002090344fd
Vidhi Nair <74731487+VidhiNair@users.noreply.github.com>
2021-05-17 10:21:57 +0100

    Add files via upload

---
+++ recipe1.js
@@ -0,0 +1,109 @@
+function recipeNo1(){
+    textSize(30)
+    fill("red")
+    text("Recipe 1: Fried Egg", windowWidth/2-800, windowHeight/2-150)
+
+    textSize(25)
+    fill("black")
+    text("Ingredients:", windowWidth/2-750, windowHeight/2-100)
+
+    textSize(20)
+    fill("black")
+    text("-Eggs", windowWidth/2-750, windowHeight/2-50)
+
+    textSize(20)
+    fill("black")
+    text("-Pepper", windowWidth/2-750, windowHeight/2-25)
+
+    textSize(20)
+    fill("black")
+    text("-Salt", windowWidth/2-750, windowHeight/2)
+
+    textSize(20)
+    fill("black")
+    text("-Oil", windowWidth/2-750, windowHeight/2+25)
+
+    textSize(30)
+    fill("red")
+    text("Process:", windowWidth/2-450, windowHeight/2-150)
+
+    textSize(15)
+    fill("black")
+    text("Step 1: Place the pan on stove & add some oil.", windowWidth/2-490, windowHeight/2-100)
+
+    textSize(15)
+    fill("black")
+    text("Step 2: Crack an egg into the pan.", windowWidth/2-490, windowHeight/2-75)
+
+    textSize(15)
+    fill("black")
+    text("Step 3: Let the egg cook for 30 seconds.", windowWidth/2-490, windowHeight/2-50)
+
+    textSize(15)
+    fill("black")
+    text("Step 4: Take the egg out of the pan & ", windowWidth/2-490, windowHeight/2-25)
+
+    textSize(15)
+    fill("black")
+    text("place it in the plate.", windowWidth/2-425, windowHeight/2)
+
+    textSize(15)
+    fill("black")
+    text("Step 5: Add salt, peper & ketchup.", windowWidth/2-490, windowHeight/2+25)
+
+}
+
+function recipeNo2(){
+  textSize(30)
+  fill("red")
+  text("Recipe 2: Pancake", windowWidth/2-800, windowHeight/2-150)
+
+  textSize(25)
+  fill("black")
+  text("Ingredients:", windowWidth/2-750, windowHeight/2-100)
+
+  textSize(20)
+  fill("black")
+  text("-Egg", windowWidth/2-750, windowHeight/2-50)
+
+  textSize(20)
+  fill("black")
+  text("-Milk", windowWidth/2-750, windowHeight/2-25)
+
+  textSize(20)
+  fill("black")
+  text("-Flour", windowWidth/2-750, windowHeight/2)
+
+  textSize(20)
+  fill("black")
+  text("-Oil", windowWidth/2-750, windowHeight/2+25)
+
+  textSize(30)
+  fill("red")
+  text("Process:", windowWidth/2-450, windowHeight/2-150)
+
+  textSize(15)
+  fill("black")
+  text("Step 1: Place all the ingredients in a bowl.", windowWidth/2-490, windowHeight/2-100)
+
+  textSize(15)
+  fill("black")
+  text("Step 2: Crack an egg into the bowl.", windowWidth/2-490, windowHeight/2-75)
+
+  textSize(15)
+  fill("black")
+  text("Step 3: Mix the ingredients together for 30 seconds.", windowWidth/2-490, windowHeight/2-50)
+
+  textSize(15)
+  fill("black")
+  text("Step 4: Place the pan on the stove & ", windowWidth/2-490, windowHeight/2-25)
+
+  textSize(15)
+  fill("black")
+  text("pour the ingredients into the pan.", windowWidth/2-425, windowHeight/2)
+
+  textSize(15)
+  fill("black")
+  text("Step 5: Cook for 30 seconds and serve! Enjoy!.", windowWidth/2-490, windowHeight/2+25)
+}
+
